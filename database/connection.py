d5e2e3da5371693580162e5a2777f5f06c8a37bf
vmgarcia18 <vmgarcia18@hotmail.com>
2020-06-08 14:43:52 -0400

    Create tables en la BD y algunos inserts

--- database/connection.py
+++ database/connection.py
@@ -3,15 +3,32 @@ from sqlite3 import Error
 
 def sql_connection():
     try:
-        conn = sqlite3.connect('test.db')
+        conn = sqlite3.connect('mamma_maglioni.db')
         return conn
     except Error:
         print(Error)
 
 def sql_table(conn):
-    cursorObj = conn.cursor()
-    cursorObj.execute("CREATE TABLE empleado(id integer PRIMARY KEY, name text)")
+    cursor = conn.cursor()
+
+    cursor.execute("create table pizza(id integer not null primary key, size text not null, precio real not null, fk_pedido integer)")
+    cursor.execute("create table ingrediente(id integer primary key, nombre text not null)")
+    cursor.execute("create table pedido(id integer primary key, nombre_cliente text not null, fecha text not null)")
+    cursor.execute("create table piz_ing(id integer primary key, precio real not null, fk_pizza integer, fk_pedido integer)")
+
+    conn.commit()
+
+def sql_insert(conn):
+    cursor = conn.cursor()
+    
+    ingredientes = [("Jamon",), ("Champinones",), ("Pimenton",), ("Doble queso",), ("Aceitunas",), ("Pepperoni",), ("Salchichon",),]
+    cursor.executemany("insert into ingrediente(nombre) values (?)", ingredientes)
+
+    pizzas = [("Personal", 10,), ("Mediana", 15,), ("Familiar", 20,),]
+    cursor.executemany("insert into pizza(size, precio) values (?, ?)", pizzas)
+
     conn.commit()
 
 conn = sql_connection()
 sql_table(conn)
+sql_insert(conn)
