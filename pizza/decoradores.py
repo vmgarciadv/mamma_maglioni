488ebef27021cf79b777d1d3cf4d3d4c62e8d240
Amelia Brito <ameliagiampaolo@gmail.com>
2020-06-16 15:51:45 -0400

    Se agrego el resto de los ingredientes

--- pizza/decoradores.py
+++ pizza/decoradores.py
@@ -51,114 +51,82 @@ class Champiñones(IngredientesDecorator):
     def informacion(self):
         return self.pizza.informacion() + "Champiñones, "
 
-"""
 class Pimenton(IngredientesDecorator):
-
     def __init__(self, pizza):
-        Pizza.__init__(self, pizza.size, pizza.precio)
+        Pizza.__init__(self, pizza.size)
+        self.pizza = pizza
 
-    def get_size(self):
-        return self.size
-
-    @overrides(IngredientesDecorator)
-    def get_precio(self):      
+    def get_precio(self):
         if self.size == "Personal":
-            return self.precio + 1.5
+            return 1.5 + self.pizza.get_precio()
         elif self.size == "Mediana":
-            return self.precio + 1.75
+            return 1.75 + self.pizza.get_precio()
         else:
-            return self.precio + 2.00
-    
-      
-    @overrides(IngredientesDecorator)
+            return 2.00 + self.pizza.get_precio()
+
     def informacion(self):
-        return self.get_size() + " con Pimentón y cuesto " + str(self.get_precio())
+        return self.pizza.informacion() + "Pimenton, "
 
 class DobleQueso(IngredientesDecorator):
-
     def __init__(self, pizza):
-        Pizza.__init__(self, pizza.size, pizza.precio)
+        Pizza.__init__(self, pizza.size)
+        self.pizza = pizza
 
-    def get_size(self):
-        return self.size
-
-    @overrides(IngredientesDecorator)
-    def get_precio(self):      
+    def get_precio(self):
         if self.size == "Personal":
-            return self.precio + 0.80
+            return 0.80 + self.pizza.get_precio()
         elif self.size == "Mediana":
-            return self.precio + 1.30
+            return 1.30 + self.pizza.get_precio()
         else:
-            return self.precio + 1.70
-    
-      
-    @overrides(IngredientesDecorator)
+            return 1.70 + self.pizza.get_precio()
+
     def informacion(self):
-        return self.get_size() + " con doble queso y cuesto " + str(self.get_precio())
+        return self.pizza.informacion() + "Doble Queso, "
 
 class Aceitunas(IngredientesDecorator):
-
     def __init__(self, pizza):
-        Pizza.__init__(self, pizza.size, pizza.precio)
+        Pizza.__init__(self, pizza.size)
+        self.pizza = pizza
 
-    def get_size(self):
-        return self.size
-
-    @overrides(IngredientesDecorator)
-    def get_precio(self):      
+    def get_precio(self):
         if self.size == "Personal":
-            return self.precio + 1.80
+            return 1.80 + self.pizza.get_precio()
         elif self.size == "Mediana":
-            return self.precio + 2.15
+            return 2.15 + self.pizza.get_precio()
         else:
-            return self.precio + 2.60
-    
-      
-    @overrides(IngredientesDecorator)
+            return 2.60 + self.pizza.get_precio()
+
     def informacion(self):
-        return self.get_size() + " con Aceitunas y cuesto " + str(self.get_precio())
+        return self.pizza.informacion() + "Aceitunas, "
 
 class Pepperoni(IngredientesDecorator):
-
     def __init__(self, pizza):
-        Pizza.__init__(self, pizza.size, pizza.precio)
+        Pizza.__init__(self, pizza.size)
+        self.pizza = pizza
 
-    def get_size(self):
-        return self.size
-
-    @overrides(IngredientesDecorator)
-    def get_precio(self):      
+    def get_precio(self):
         if self.size == "Personal":
-            return self.precio + 1.25
+            return 1.25 + self.pizza.get_precio()
         elif self.size == "Mediana":
-            return self.precio + 1.70
+            return 1.70 + self.pizza.get_precio()
         else:
-            return self.precio + 1.90
-    
-      
-    @overrides(IngredientesDecorator)
+            return 1.90 + self.pizza.get_precio()
+
     def informacion(self):
-        return self.get_size() + " con Aceitunas y cuesto " + str(self.get_precio())
+        return self.pizza.informacion() + "Pepperoni, "
 
 class Salchichon(IngredientesDecorator):
-
     def __init__(self, pizza):
-        Pizza.__init__(self, pizza.size, pizza.precio)
+        Pizza.__init__(self, pizza.size)
+        self.pizza = pizza
 
-    def get_size(self):
-        return self.size
-
-    @overrides(IngredientesDecorator)
-    def get_precio(self):      
+    def get_precio(self):
         if self.size == "Personal":
-            return self.precio + 1.60
+            return 1.60 + self.pizza.get_precio()
         elif self.size == "Mediana":
-            return self.precio + 1.85
+            return 1.85 + self.pizza.get_precio()
         else:
-            return self.precio + 2.10
-    
-      
-    @overrides(IngredientesDecorator)
+            return 2.10 + self.pizza.get_precio()
+
     def informacion(self):
-        return self.get_size() + " con Salchichón y cuesto " + str(self.get_precio())
-        """
+        return self.pizza.informacion() + "Salchichon, "
--- pizza/principal.py
+++ pizza/principal.py
@@ -1,9 +1,10 @@
 from pizza import Pizza, PizzaPersonal, PizzaMediana, PizzaFamiliar
-from decoradores import IngredientesDecorator, Jamon, Champiñones #, Pimenton, DobleQueso, Aceitunas, Pepperoni, Salchichon
+from decoradores import IngredientesDecorator, Jamon, Champiñones, Pimenton, DobleQueso, Aceitunas, Pepperoni, Salchichon
 
 
 
 pizza = PizzaFamiliar()
 pizza = Jamon(pizza)
 pizza = Champiñones(pizza)
+pizza = Salchichon(pizza)
 print(pizza.informacion() + "y cuesto " +str(pizza.get_precio()))
